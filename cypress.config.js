578cf0cf28d3cbcf9f29cdd447d859f767aacdc3
Anatoliy <a.zubovych@gmail.com>
2023-09-17 14:05:27 +0300

    config(s)

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    nonGlobalStepDefinitions: true,
+    stepDefinitions: "cypress/e2e/verify"
+  },
+})
